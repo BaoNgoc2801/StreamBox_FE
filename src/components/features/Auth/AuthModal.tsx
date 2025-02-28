2e80a629d765e9919190b1da13188c4ccfcc209c
BaoNgoc2801 <baongoc2801np@gmail.com>
2025-02-28 21:06:07 +0700

    Animation for auth popup + logo

--- src/components/features/Auth/AuthModal.tsx
+++ src/components/features/Auth/AuthModal.tsx
@@ -12,7 +12,7 @@ const AuthModal: React.FC<AuthModalProps> = ({ onClose }) => {
 
     return (
         <motion.div
-            className="fixed inset-0 flex items-center justify-center bg-opacity-50 z-50"
+            className="fixed inset-0 flex items-center justify-center bg-opacity-50 z-50 "
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
@@ -134,7 +134,7 @@ const AuthModal: React.FC<AuthModalProps> = ({ onClose }) => {
                             autoPlay
                             loop
                             muted
-                            className="w-full h-full object-cover absolute top-0 left-0 opacity-30"
+                            className="w-full h-full object-cover absolute top-0 left-0 opacity-50"
                         />
                         <AnimatePresence mode="wait">
                             <motion.div
